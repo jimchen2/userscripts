80eb0a18481c11d65f6e2bafb2b8f69b4abe2df7
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2025-02-20 04:39:17 +0800

    Update script.js

--- old-reddit-redirect/script.js
+++ old-reddit-redirect/script.js
@@ -6,11 +6,13 @@
 // @author       Jim Chen
 // @match        *://*.reddit.com/*
 // @exclude      *://old.reddit.com/*
+// @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  if (window.location.hostname.indexOf("old.reddit.com") === -1) {
-    window.location.replace(window.location.href.replace(/\/\/(www\.)?reddit\.com/, "//old.reddit.com"));
-  }
+(function() {
+    'use strict';
+    if (window.location.hostname.indexOf('old.reddit.com') === -1) {
+        window.location.replace(window.location.href.replace(/\/\/(www\.)?reddit\.com/, '//old.reddit.com'));
+    }
 })();
