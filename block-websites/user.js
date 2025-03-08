bec57ceb6a6c5a799de496b9f734f7e60295dcf6
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2025-03-09 03:27:19 +0800

    Update user.js

--- block-websites/user.js
+++ block-websites/user.js
@@ -7,11 +7,21 @@
 // @match        *://*.instagram.com/*
 // @match        *://*.weibo.com/*
 // @match        *://*.douban.com/*
-// @match        *://*.bilibili.com/*
-// @match        *://*.baidu.com/*
+// @match        *://*.tieba.baidu.com/*
+// @match        *://*.tiebac.baidu.com/*
+// @match        *://*.weifan.baidu.com/*
+// @match        *://*.nani.baidu.com/*
+// @match        *://*.reddit.com/r/China_irl/*
 // @match        *://*.huaren.us/*
 // @match        *://*.tiktok.com/*
+// @match        *://*.pincong.rocks/*
 // @match        *://*.douyin.com/*
+// @match        *://*.mastodon.social/*
+// @match        *://*.bsky.app/*
+// @match        *://*.truthsocial.com/*
+// @match        *://*.kiwifarms.st/*
+// @match        *://*.infowars.com/*
+// @match        *://*.zombsroyale.io/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
@@ -19,4 +29,6 @@
 (function () {
   "use strict";
   window.stop();
+
+  alert("WEBSITE BLOCKED\n\nSite blocked by Userscript because it contains extremist contents.\n\nPlease choose healthy alternatives.");
 })();
