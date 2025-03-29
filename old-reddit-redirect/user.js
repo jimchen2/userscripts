0d914ecef900d39fd60e7e7799a962b9abd547bf
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2025-03-29 15:27:49 +0800

    Update user.js

--- old-reddit-redirect/user.js
+++ old-reddit-redirect/user.js
@@ -6,12 +6,19 @@
 // @author       Jim Chen
 // @match        *://*.reddit.com/*
 // @exclude      *://old.reddit.com/*
+// @exclude      *://www.reddit.com/media*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    // Don't redirect if it's a media URL
+    if (window.location.pathname.startsWith('/media')) {
+        return;
+    }
+
+    // Redirect to old.reddit.com if not already there
     if (window.location.hostname.indexOf('old.reddit.com') === -1) {
         window.location.replace(window.location.href.replace(/\/\/(www\.)?reddit\.com/, '//old.reddit.com'));
     }
