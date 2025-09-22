f9862363cd4407ae8e6938b434af761e36c126ea
Jim Chen <jimchen4214@gmail.com>
2025-09-22 21:32:38 +0800

    Updates

--- youtube-dual-subtitles.user.js
+++ youtube-dual-subtitles.user.js
@@ -47,6 +47,34 @@
     handleVideoNavigation();
   }
 
+  async function handleVideoNavigation() {
+    console.log("[DUAL SUBS] FIRED");
+    removeSubs();
+
+    const languageCheckPassed = await checkLanguageCode();
+    if (!languageCheckPassed) return;
+
+    const subtitleURL = await extractSubtitleUrl();
+    if (subtitleURL == null) return;
+    const subtitleButton = document.querySelector(".ytp-subtitles-button");
+    if (subtitleButton && subtitleButton.getAttribute("aria-pressed") === "true") subtitleButton.click();
+    const url = new URL(subtitleURL);
+    if (!url.searchParams.has("kind")) url.searchParams.set("kind", "asr");
+    if (url.searchParams.has("fmt")) {
+      url.searchParams.set("fmt", "vtt");
+    } else {
+      url.searchParams.set("fmt", "vtt");
+    }
+    url.searchParams.delete("tlang");
+    const transUrl = new URL(url);
+    transUrl.searchParams.set("tlang", "en");
+    const transSub = transUrl.toString();
+    console.log(`[DUAL SUBS] transSub ${transSub}`);
+    await addOneSubtitle(transSub);
+    console.log(`[DUAL SUBS] subtitleURL ${url.toString()}`);
+    await addOneSubtitle(url.toString());
+  }
+
   function checkLanguageCode() {
     return new Promise((resolve) => {
       let attempts = 0;
@@ -84,33 +112,6 @@
         }
       }, 1000);
     });
-  }
-  async function handleVideoNavigation() {
-    console.log("[DUAL SUBS] FIRED");
-    removeSubs();
-
-    const languageCheckPassed = await checkLanguageCode();
-    if (!languageCheckPassed) return;
-
-    const subtitleURL = await extractSubtitleUrl();
-    if (subtitleURL == null) return;
-    const subtitleButton = document.querySelector(".ytp-subtitles-button");
-    if (subtitleButton && subtitleButton.getAttribute("aria-pressed") === "true") subtitleButton.click();
-    const url = new URL(subtitleURL);
-    if (!url.searchParams.has("kind")) url.searchParams.set("kind", "asr");
-    if (url.searchParams.has("fmt")) {
-      url.searchParams.set("fmt", "vtt");
-    } else {
-      url.searchParams.set("fmt", "vtt");
-    }
-    url.searchParams.delete("tlang");
-    const transUrl = new URL(url);
-    transUrl.searchParams.set("tlang", "en");
-    const transSub = transUrl.toString();
-    console.log(`[DUAL SUBS] transSub ${transSub}`);
-    await addOneSubtitle(transSub);
-    console.log(`[DUAL SUBS] subtitleURL ${url.toString()}`);
-    await addOneSubtitle(url.toString());
   }
 
   async function extractSubtitleUrl() {
