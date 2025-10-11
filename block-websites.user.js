7565a779bbcda2bdab8062cf3740db1bf2a507c0
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2025-10-11 22:04:51 +0800

    Update block-websites.user.js

--- block-websites.user.js
+++ block-websites.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Block Websites
 // @namespace    http://tampermonkey.net/
-// @version      5.0
+// @version      5.1
 // @license      Unlicense
 // @description  Block Harmful Websites
 // @author       Jim Chen
@@ -23,7 +23,7 @@
   } else blockSite();
 
   function blockSite() {
-    window.location.replace("https://jw.ustc.edu.cn");
+    window.location.replace("https://docs.google.com/document/d/1GIW1oz-ttAG3B08usdbz758iCgShPd4DWcBis_IBbgM/edit?tab=t.0");
     alert(
       "WEBSITE BLOCKED\n\nSite blocked by Userscript because it contains extremist contents.\n\nPlease choose healthy alternatives."
     );
